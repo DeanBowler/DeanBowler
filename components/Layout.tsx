fd002185e6fb2f6772199c484e4f558ad1225ad2
Dean Bowler <dean.bowler@boomin.com>
2020-12-30 22:18:49 +0000

    Add favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -100,8 +100,9 @@ export const Layout = ({ children, title = 'Dean Bowler' }: Props) => {
           <title>{title}</title>
           <meta charSet="utf-8" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css?family=Raleway:100,200,400,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;400;700&display=swap"
             rel="stylesheet"
           />
         </Head>
